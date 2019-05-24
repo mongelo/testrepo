e4f8f110c6d3f962bc5a66460f0e99c7381c5ac1
Jens Roderus <mongelonas@hotmail.com>
2019-05-24 14:08:16 +0200

    111

--- ConsoleApp1/Program - Copy.cs
+++ ConsoleApp1/Program - Copy.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");dddddd*dd
+            Console.WriteLine("Hello World!");dddddd*dds
         }
     }
 }
